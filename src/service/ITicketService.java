62a1ff27897478efd6ef20157265a8be67c7d3c1
2ys2 <2ys2bau@gmail.com>
2023-05-16 18:27:57 +0900

    cotroller

--- src/service/ITicketService.java
+++ src/service/ITicketService.java
@@ -2,7 +2,7 @@ package service;
 
 public interface ITicketService {
 	void ticketing();
-	void ticketHistory();
-	void ticketingCancel();
+	void ticketHistory( String str );
+	void ticketingCancel( String str );
 	void showScreens();
 }
