c3e834c512ec9fca6c86cd97a9adc019057f943b
grandfark <118947488+grandfark@users.noreply.github.com>
2023-05-20 19:50:28 +0900

    Update MemberController.java
    sha 256 분리 마무리

--- src/controll/MemberController.java
+++ src/controll/MemberController.java
@@ -360,24 +360,7 @@ public class MemberController implements IMemberService {
             
         }
     }
-
-    public class SHA256 {
-        public String encrypt(String text) throws NoSuchAlgorithmException {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(text.getBytes());
-            return bytesToHex(md.digest());
-        }
-
-        private String bytesToHex(byte[] bytes) {
-            StringBuilder builder = new StringBuilder();
-            for (byte b : bytes) {
-                builder.append(String.format("%02x", b));
-            }
-            return builder.toString();
-        }
-
-    }
-
+    
     private void menu() {
         sb.setLength(0);
         sb.append("───────────────────────────────────────────────").append("\n");
